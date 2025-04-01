77de6846b7ff96ba611781359aef4325826024af
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-04-01 15:33:37 +0100

    GetCommandByName working

--- core/config/config_test.go
+++ core/config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/eefenn/eefenn-cli/cmd"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -21,12 +22,18 @@ func TestGetCurrentConfig(t *testing.T) {
 
 func TestConfig_AddCommand(t *testing.T) {
 	currentConfig, err := GetCurrentConfig()
-	if err != nil {
-		return
-	}
+	require.NoError(t, err)
 
 	err = currentConfig.AddCommand(testCMD)
 	require.NoError(t, err)
+}
+
+func TestConfig_GetCommandByName(t *testing.T) {
+	currentConfig, err := GetCurrentConfig()
+	require.NoError(t, err)
+	_, result, err := currentConfig.GetCommandByName(testCMD.Name)
+	require.NoError(t, err)
+	assert.Equal(t, testCMD, *result)
 }
 
 func TestConfig_RemoveCommandByName(t *testing.T) {
