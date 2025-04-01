ba5aadbc751efeb6d295176fc3d88a6c8cec93c8
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-04-01 12:20:57 +0100

    add function added to config.go

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/eefenn/eefenn-cli/cmd-config"
 	"os"
 )
@@ -9,7 +10,11 @@ import (
 const EefennCLIConfig = "/usr/lib/eefenn-cli/eefenn-cli.config.json"
 
 type ConfigInterface interface {
+	GetCommandByName(name string) (*int, *cmd_config.Command, error)
+
 	Update() error
+
+	AddCommand(subcommand cmd_config.Command)
 }
 
 type Config struct {
@@ -37,19 +42,36 @@ func GetCurrentConfig() (Config, error) {
 	return config, nil
 }
 
-func (c *Config) getCommandByName(name string) cmd_config.Command {
-	var targetIndex int
+// GetCommandByName
+//
+// Get a that exists in the config file command by the name of the command.
+//
+// Returns the index of the command, the Command struct for
+// the command, and error status.
+func (c *Config) GetCommandByName(name string) (*int, *cmd_config.Command, error) {
+	var p_cmd *cmd_config.Command
+	var p_commandIndex *int
 
 	// find the index of the item whose Name matches the parameter 'name'
 	for index, sc := range c.Commands {
 		if sc.Name == name {
-			targetIndex = index
+			p_commandIndex = &index
+			p_cmd = &sc
 		}
 	}
 
-	return c.Commands[targetIndex]
+	if p_cmd == nil {
+		return nil, nil, fmt.Errorf("Could not find command with the name: %s\n", name)
+	} else {
+		return p_commandIndex, p_cmd, nil
+	}
 }
 
+// Update
+//
+// Write the contents of a Config object to the config file.
+//
+// Returns error status.
 func (c *Config) Update() error {
 	jsonData, err := json.MarshalIndent(c, "", "	")
 	if err != nil {
@@ -61,5 +83,14 @@ func (c *Config) Update() error {
 		return err
 	}
 
+	return nil
+}
+
+// AddCommand
+//
+// Update /usr/lib/eefenn-cli/eefenn-cli.config.json with
+// marshalled subcommand data.
+func (c *Config) AddCommand(subcommand cmd_config.Command) error {
+	c.Commands = append(c.Commands, subcommand)
 	return nil
 }
