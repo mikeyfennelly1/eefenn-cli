be4873497d23d60d82bbb69fc36257aeb352e8d8
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-04-01 09:23:31 +0100

    parsing yml

--- cmd-config/create_cmd_from_yml.go
+++ cmd-config/create_cmd_from_yml.go
@@ -1,5 +1,42 @@
 package cmd_config
 
-type CommandConfig struct {
-	command Subcommand
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+)
+
+type Command struct {
+	// array of filepaths that the script needs to run
+	Needs []string `yaml:"needs"`
+
+	// alias of the script
+	Name string `yaml:"name"`
+
+	// the script which the command is an alias for
+	Script string `yaml:"script"`
+
+	// description for what the script does
+	Description string `yaml:"description"`
+
+	// the arguments to the command
+	Args []struct {
+		Name        string `yaml:"string"`
+		Description string `yaml:"string"`
+	}
+}
+
+func GetCommandFromYml() {
+	yamlData, err := os.ReadFile("config.yaml")
+
+	var config Command
+	err = yaml.Unmarshal([]byte(yamlData), &config)
+	if err != nil {
+		fmt.Println("Error parsing YAML:", err)
+		return
+	}
+
+	// Print parsed struct
+	fmt.Printf("Parsed struct: %+v\n", config)
+
 }
---
+++ cmd-config/create_cmd_from_yml_test.go
@@ -0,0 +1,11 @@
+package cmd_config
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetCommandFromYml(t *testing.T) {
+	GetCommandFromYml()
+	fmt.Println("nothin")
+}
